c67a079183b4cde438459351538accac266a1b7f
surajpanker82 <surajpanker82@gmail.com>
2022-03-28 13:56:02 +0530

    updated cpp file

--- AND xor OR.cpp
+++ AND xor OR.cpp
@@ -6,13 +6,6 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-/*
- * Complete the 'andXorOr' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY a as parameter.
- */
-
 int andXorOr(vector<int> arr) {
     
     stack<int> s;// we store minimum in stack
